5828b2a1aca98045afdc51868d031a25d970cf0a
nakitamray <nakitamray@gmail.com>
2023-01-31 21:50:33 -0800

    File Setup

--- narwhal-dashboard/src/App.js
+++ narwhal-dashboard/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+import Timer from './Components/timer.js';
+import Grid from './Components/grid.js';
+import Auto from './Components/auto.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
