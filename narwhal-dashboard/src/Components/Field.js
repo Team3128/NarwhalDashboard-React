63c13100337b3c0ed3d75b90c352cec19f8a75a0
Lucas Han <lucas.joel.han@gmail.com>
2024-12-02 17:24:04 -0800

    explicit pose display

--- narwhal-dashboard/src/Components/Field.js
+++ narwhal-dashboard/src/Components/Field.js
@@ -24,6 +24,11 @@ const Field = (props) => {
     return(
         <>
             <img id = "field" src = {field}></img>
+            <p style ={{
+                marginTop: "400px"
+            }}>
+                ({props.robotX} m, {props.robotY} m, {props.robotYaw} deg)
+            </p>
             <img id = "robot" src = {robot} style = {{
                 position: "relative",
                 width: robotWidth * pixelsPerMeter + "px",
